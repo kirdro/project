7d90d99bcd14a4f17370c0ea9455472485bce1ef
abramius-evolutional <ahiles.ru@gmail.com>
2014-01-29 22:08:04 +0400

    Забыл постгрес активировать

--- project/settings.py
+++ project/settings.py
@@ -57,22 +57,22 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-     # 'default': {
-     #     'ENGINE':'django.db.backends.postgresql_psycopg2',
-     #     'NAME': 'mydb',
-     #     'USER': 'postgres',
-     #     'PASSWORD': 'kirql',
-     #     'HOST': 'localhost',
-     #     'PORT': '5432',
-     # }
-   'default': {
-       'ENGINE':'django.db.backends.sqlite3',
-       'NAME': 'mydb.sqlite',
-       'USER': '',
-       'PASSWORD': '',
-       'HOST': '',
-       'PORT': '',
-   }
+     'default': {
+         'ENGINE':'django.db.backends.postgresql_psycopg2',
+         'NAME': 'mydb',
+         'USER': 'postgres',
+         'PASSWORD': 'kirql',
+         'HOST': 'localhost',
+         'PORT': '5432',
+     }
+   # 'default': {
+   #     'ENGINE':'django.db.backends.sqlite3',
+   #     'NAME': 'mydb.sqlite',
+   #     'USER': '',
+   #     'PASSWORD': '',
+   #     'HOST': '',
+   #     'PORT': '',
+   # }
 }
 
 # Internationalization
